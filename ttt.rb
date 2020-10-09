bdd5bf8cdf02dbe44d7aba4ba0b534fbd84ddc59
swizel <swizel.sarah@gmail.com>
2020-10-09 10:54:38 +0100

    + comments

--- ttt.rb
+++ ttt.rb
@@ -3,6 +3,7 @@
 
 @board = [["1","2","3"],["4","5","6"],["7","8","9"]]
 
+# draws the board
 def draw_grid
   @board.length.times { |i|
     puts "    #{@board[i][0]} |   #{@board[i][1]} |   #{@board[i][2]} ".center(48)
@@ -11,6 +12,7 @@ def draw_grid
     end
   }
 end
+
 
 def get_move
   draw_grid()
@@ -21,6 +23,7 @@ def get_move
     puts "Bye!"
     exit
   end
+  # input can only be the number that is still on the board (i.e empty spaces)
   while !(@board.flatten.include?(move)) do
     puts "Please enter a valid number"
     move = gets.chomp
@@ -29,9 +32,11 @@ def get_move
 end
 
 def update_board(move, player)
+  #find the space through the number
   @board.each { |x|
     x.each { |y|
       if y == move
+        #reference by index and change it
         player == "1" ? (@board[(@board.index(x))][x.index(y)] = "X") : (@board[(@board.index(x))][x.index(y)] = "O")
       end
     }
@@ -39,18 +44,21 @@ def update_board(move, player)
 end
 
 def check(player)
-  @board.each { |x|
+  #checking for an in identical row
+  @board.each do |x|
     if x.uniq.length == 1
       puts "Player #{player} wins".center(50, "~")
       return true
     end
-  }
+  end
+  #checking for identical column
   for i in 0..2 do
     if @board[0][i] == @board[1][i] && @board[1][i]== @board[2][i]
       puts "Player #{player} wins".center(50, "~")
       return true
     end
   end
+  #checking for across from either side
   if (@board[0][0] == @board[1][1] && @board[1][1]== @board[2][2]) || (@board[0][2] == @board[1][1] && @board[1][1] == @board[2][0])
     puts "Player #{player} wins".center(50, "~")
     return
@@ -61,13 +69,15 @@ end
 flag = false
 turn = 1
 while flag == false do
+  #when turn is odd its player 1
   turn % 2 != 0 ? (player = "1") : (player = "2")
   puts "Turn : Player #{player}".center(50)
   move = get_move
   update_board(move,player)
   flag = check(player)
   turn += 1
-  if turn > 8
+  # check if draw
+  if @board.flatten.uniq.length == 2
     puts "It's a draw".center(50,"~")
     flag = true
   end
